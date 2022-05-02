93976180b315dc973df4dd66c23e619ced1f4358
alejandro <alekgy@gmail.com>
2022-05-02 13:43:05 -0500

    codigo escrito sobre las funcionalidades basicas de Django y como usarlas

---
+++ premios_platzi/premios_platziapp/polls/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
+from django.urls import reverse
+from django.views import generic
+
+# def index(request):
+#     lastest_questions_list = Question.objects.all()
+#     return render(request, "polls/index.html", {
+#         "latest_questions_list": lastest_questions_list
+#     })
+
+
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/detail.html", {
+#         "question": question
+#     })
+
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/results.html", {
+#         "question": question
+#     })
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_questions_list"
+
+    def get_queryset(self):
+        """return the last publish question"""
+        return Question.objects.order_by("-pub_date")[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = "polls/result.html"
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {
+            "question": question,
+            "error_message": "no elegiste una respuesta"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
